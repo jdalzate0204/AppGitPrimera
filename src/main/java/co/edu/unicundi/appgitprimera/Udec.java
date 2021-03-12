51b1c02758cc80b8bce3427e15353c5c23a7543d
jdalzate0204 <james.alzate22@hotmail.com>
2021-03-12 07:44:00 -0500

    se agrega constructor vacio UDEC

---
+++ src/main/java/co/edu/unicundi/appgitprimera/Udec.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.unicundi.appgitprimera;
+
+/**
+ *
+ * @author acer
+ */
+public class Udec {
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the ubicacion
+     */
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    /**
+     * @param ubicacion the ubicacion to set
+     */
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+    private String nombre;
+    private String ubicacion;
+
+    public Udec(String nombre, String ubicacion) {
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+    }
+    
+    
+}
